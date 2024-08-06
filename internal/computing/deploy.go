2d54c3957c1287c011ab34a463011324114d0ac1
sonic <longzhang>
2024-08-06 17:54:05 +0800

    add log

--- internal/computing/deploy.go
+++ internal/computing/deploy.go
@@ -485,7 +485,10 @@ func (d *Deploy) ModelInferenceToK8s() error {
 	return nil
 }
 
-func (d *Deploy) DeploySshTaskToK8s(hostPort int32) error {
+func (d *Deploy) DeploySshTaskToK8s(nodePort int32) error {
+	fmt.Printf("sshkey: %v", d.sshKey)
+	fmt.Printf("nodeport: %d", nodePort)
+
 	k8sService := NewK8sService()
 	deployment := &appV1.Deployment{
 		TypeMeta: metaV1.TypeMeta{
@@ -533,12 +536,16 @@ func (d *Deploy) DeploySshTaskToK8s(hostPort int32) error {
 		return fmt.Errorf("failed to get pod, error: %v", err)
 	}
 
+	fmt.Printf("podName: %v", podName)
+
 	podCmd := []string{"sh", "-c", fmt.Sprintf("echo '%s' > /root/.ssh/authorized_keys", d.sshKey)}
+
+	fmt.Printf("podCmd: %s", podCmd)
 	if err = k8sService.PodDoCommand(d.k8sNameSpace, podName, "", podCmd); err != nil {
 		return fmt.Errorf("failed to add sshkey, error: %v", err)
 	}
 
-	createService, err := k8sService.CreateServiceByNodePort(context.TODO(), d.k8sNameSpace, d.taskUuid, 22, hostPort)
+	createService, err := k8sService.CreateServiceByNodePort(context.TODO(), d.k8sNameSpace, d.taskUuid, 22, nodePort)
 	if err != nil {
 		return fmt.Errorf("failed to create service, error: %w", err)
 	}
