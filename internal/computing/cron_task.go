c8f04f2882ebaaf6741522ef63e95c83fe79a65c
sonic <longzhang>
2024-08-21 18:41:27 +0800

    fix the bug of cron task deleting job

--- internal/computing/cron_task.go
+++ internal/computing/cron_task.go
@@ -239,7 +239,7 @@ func (task *CronTask) watchNameSpaceForDeleted() {
 
 func (task *CronTask) cleanImageResource() {
 	c := cron.New(cron.WithSeconds())
-	c.AddFunc("@every 24h", func() {
+	c.AddFunc("* 0/30 * * * ?", func() {
 		defer func() {
 			if err := recover(); err != nil {
 				logs.GetLogger().Errorf("cleanImageResource catch panic error: %+v", err)
@@ -478,7 +478,7 @@ func (task *CronTask) checkJobReward() {
 
 func (task *CronTask) getUbiTaskReward() {
 	c := cron.New(cron.WithSeconds())
-	c.AddFunc("* 0/10 * * ?", func() {
+	c.AddFunc("* 0/10 * * * ?", func() {
 		defer func() {
 			if err := recover(); err != nil {
 				logs.GetLogger().Errorf("task job: [GetUbiTaskReward], error: %+v", err)
