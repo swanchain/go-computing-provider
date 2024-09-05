975271f3c2c0bc7d3d54f879de222305a301f70f
sonic <longzhang>
2024-09-05 15:27:06 +0800

    add log

--- internal/computing/cron_task.go
+++ internal/computing/cron_task.go
@@ -62,7 +62,7 @@ func checkClusterNetworkPolicy() {
 
 	netset, err := NewK8sService().GetGlobalNetworkSet(models.NetworkNetset)
 
-	logs.GetLogger().Infof("netset: %v, err: %v", err)
+	logs.GetLogger().Infof("netset: %v, err: %v", netset, err)
 	logs.GetLogger().Infof("netset: %t, err: %t", netset == nil, err == nil)
 
 	if err != nil {
