603c0bc17e567c15ec10b42521bb778507927532
sonic <longzhang>
2024-12-09 11:46:20 +0800

    update corn time

--- internal/computing/cron_task.go
+++ internal/computing/cron_task.go
@@ -472,7 +472,8 @@ func (task *CronTask) setFailedUbiTaskStatus() {
 
 func (task *CronTask) checkJobReward() {
 	c := cron.New(cron.WithSeconds())
-	c.AddFunc("* * 0/20 * * ?", func() {
+	//c.AddFunc("* * 0/20 * * ?", func() {
+	c.AddFunc("0 */10 * * * ?", func() {
 		defer func() {
 			if err := recover(); err != nil {
 				logs.GetLogger().Errorf("task job: [checkJobReward], error: %+v", err)
