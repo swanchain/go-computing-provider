afc23448638ae1b777ca742a136fc5d02e2f4cf1
sonic <longzhang>
2025-01-14 10:04:41 +0800

    add log

--- internal/computing/ubi_service.go
+++ internal/computing/ubi_service.go
@@ -1719,7 +1719,7 @@ func GetCpBalance() {
 		SequencerBalance: roundToSixDecimal(sequencerBalance),
 	}
 	cpBalanceEntity, err := NewCpBalanceService().GetCpBalance(cpAccountAddress)
-	fmt.Printf("cpBalanceEntity: %v, err: %v", cpBalanceEntity, err)
+	fmt.Printf("cpBalanceEntity: %v, err: %v, %t", cpBalanceEntity, err, cpBalanceEntity == nil)
 	if err != nil || cpBalanceEntity == nil {
 		err = NewCpBalanceService().SaveCpBalance(cpBalance)
 	} else {
